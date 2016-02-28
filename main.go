f5f7bcbd8125e1c8c049d09dc1bf96fc48f0c02f
coderobe <robin@broda.me>
2016-02-28 01:33:22 +0100

    systray test

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"github.com/getlantern/systray"
+	"github.com/shex-project/shex/icon"
 )
 
-func main () {
-  fmt.Println("this is shex")
+func main() {
+	systray.Run(onReady)
+	fmt.Println("this is shex")
+}
+
+func onReady() {
+	// systray init
+	systray.SetIcon(icon.Data) // do we need an icon for this to work?
+	systray.SetTitle("shex")
+	systray.SetTooltip("this is the shex systray. woo")
+	// menu items
+	mScreenshotDesktop := systray.addMenuItem("Screenshot entire desktop", "")
+	mSettings := systray.addMenuItem("Settings", "Change some stuff")
+	mQuit := systray.addMenuItem("Quit", "Exit this crap")
+
+	// do we need different goroutines for each menu item?
+
+	go func() {
+		<-mScreenshotDesktop.ClickedCh
+		// TODO: automagically close systray and take a screenshot
+		fmt.Println("ScreenshotDesktop") // TODO: remove debug output
+	}()
+
+	go func() {
+		<-mSettings
+		// TODO: open settings dialog
+		//   - can change keybinds
+		//   - can change upload server
+		//   - other settings?
+		fmt.Println("SettingsDialog") // TODO: remove debug output
+	}()
+
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+		fmt.Println("Exiting") // TODO: make cooler???
+	}()
 }
