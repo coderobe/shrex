927e6eee8ca514e4bb3bc8b881d5ef71da28408c
coderobe <robin@broda.me>
2016-02-28 02:44:58 +0100

    Updated: Removed gofmt and all other go plugins entirely, reset this thing to my coding style.

--- main.go
+++ main.go
@@ -1,49 +1,48 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
   "os"
-
-  "github.com/getlantern/systray"
-  "github.com/shex-project/shex/icon"
+	"github.com/getlantern/systray"
+	"github.com/shex-project/shex/icon"
 )
 
 func main() {
-  systray.Run(onReady)
-  fmt.Println("this is shex")
+	systray.Run(onReady)
+	fmt.Println("this is shex")
 }
 
 func onReady() {
-  // systray init
-  systray.SetIcon(icon.Data) // do we need an icon for this to work?
-  systray.SetTitle("shex")
-  systray.SetTooltip("this is the shex systray. woo")
-  // menu items
-  mScreenshotDesktop := systray.AddMenuItem("Screenshot entire desktop", "")
-  mSettings := systray.AddMenuItem("Settings", "Change some stuff")
-  mQuit := systray.AddMenuItem("Quit", "Exit this crap")
+	// systray init
+	systray.SetIcon(icon.Data) // do we need an icon for this to work?
+	systray.SetTitle("shex")
+	systray.SetTooltip("this is the shex systray. woo")
+	// menu items
+	mScreenshotDesktop := systray.AddMenuItem("Screenshot entire desktop", "")
+	mSettings := systray.AddMenuItem("Settings", "Change some stuff")
+	mQuit := systray.AddMenuItem("Quit", "Exit this crap")
 
-  // do we need different goroutines for each menu item?
+	// do we need different goroutines for each menu item?
 
-  go func() {
-    <-mScreenshotDesktop.ClickedCh
-    // TODO: automagically close systray and take a screenshot
-    fmt.Println("ScreenshotDesktop") // TODO: remove debug output
-  }()
+	go func() {
+		<-mScreenshotDesktop.ClickedCh
+		// TODO: automagically close systray and take a screenshot
+		fmt.Println("ScreenshotDesktop") // TODO: remove debug output
+	}()
 
-  go func() {
-    <-mSettings.ClickedCh
-    // TODO: open settings dialog
-    //   - can change keybinds
-    //   - can change upload server
-    //   - other settings?
-    fmt.Println("SettingsDialog") // TODO: remove debug output
-  }()
+	go func() {
+		<-mSettings.ClickedCh
+		// TODO: open settings dialog
+		//   - can change keybinds
+		//   - can change upload server
+		//   - other settings?
+		fmt.Println("SettingsDialog") // TODO: remove debug output
+	}()
 
-  go func() {
-    <-mQuit.ClickedCh
-    systray.Quit()
-    os.Exit(0)
-    fmt.Println("EXITS WITH SOME SICK KICKFLIPS BRAH")
-  }()
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+		os.Exit(0)
+		fmt.Println("EXITS WITH SOME SICK KICKFLIPS BRAH")
+	}()
 }
